577d79d0b2783d4430927763a9535fbb54910405
malac <malac@MalachysPC.attlocal.net>
2020-05-14 16:37:36 -0400

    Malachy's Commit

--- TileGame/src/dev/codenmore/tilegame/Launcher.java
+++ TileGame/src/dev/codenmore/tilegame/Launcher.java
@@ -6,6 +6,6 @@ public class Launcher {
 	public static void main(String[] args) {
 		Game game = new Game("Tile Game",750,500);
 		game.start();
-		System.out.println("the game");
+		System.out.println("Malachy is gay");
 	}
 }
